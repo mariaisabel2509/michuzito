77dea773e5d6813d8287e880ae094cb2ebb2e872
Tu Nombre <isabelromero2509@gmail.com>
2026-09-06 21:53:01 -0500

    Corregir llave de cierre faltante

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,9 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-public function boot(): void
-{
-    if (env('APP_ENV') === 'production') {
-        \URL::forceScheme('https');
+    public function boot(): void
+    {
+        if (env('APP_ENV') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
